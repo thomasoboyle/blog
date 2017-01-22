1df1c30f4e687c6d468361ccc5aa25365c9ca779
Thomas O'Boyle <thomashoboyle@gmail.com>
2017-01-22 11:56:11 -0800

    Adds update to article controller to complete editing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to  article_path(@article)
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
